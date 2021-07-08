3871cefe3c7a47edd34a9cd0f25ecdc786e9c233
Taha <76162807+tahadumaan@users.noreply.github.com>
2021-07-08 08:43:33 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace AnıtArayüz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlNav = new System.Windows.Forms.Panel();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnIO = new System.Windows.Forms.Button();
+            this.btnParameters = new System.Windows.Forms.Button();
+            this.btnLog = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.roundedButton1 = new AnıtArayüz.RoundedButton();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(34)))), ((int)(((byte)(50)))));
+            this.panel1.Controls.Add(this.pnlNav);
+            this.panel1.Controls.Add(this.btnSettings);
+            this.panel1.Controls.Add(this.btnIO);
+            this.panel1.Controls.Add(this.btnParameters);
+            this.panel1.Controls.Add(this.btnLog);
+            this.panel1.Controls.Add(this.btnHome);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(186, 577);
+            this.panel1.TabIndex = 0;
+            // 
+            // pnlNav
+            // 
+            this.pnlNav.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(149)))));
+            this.pnlNav.Location = new System.Drawing.Point(0, 193);
+            this.pnlNav.Name = "pnlNav";
+            this.pnlNav.Size = new System.Drawing.Size(3, 100);
+            this.pnlNav.TabIndex = 1;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnSettings.FlatAppearance.BorderSize = 0;
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSettings.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(240)))), ((int)(((byte)(255)))));
+            this.btnSettings.Location = new System.Drawing.Point(0, 527);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(186, 50);
+            this.btnSettings.TabIndex = 5;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
+            this.btnSettings.Leave += new System.EventHandler(this.btnSettings_Leave);
+            // 
+            // btnIO
+            // 
+            this.btnIO.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnIO.FlatAppearance.BorderSize = 0;
+            this.btnIO.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIO.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIO.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(240)))), ((int)(((byte)(255)))));
+            this.btnIO.Location = new System.Drawing.Point(0, 294);
+            this.btnIO.Name = "btnIO";
+            this.btnIO.Size = new System.Drawing.Size(186, 50);
+            this.btnIO.TabIndex = 4;
+            this.btnIO.Text = "Input/Output";
+            this.btnIO.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnIO.UseVisualStyleBackColor = true;
+            this.btnIO.Click += new System.EventHandler(this.btnIO_Click);
+            this.btnIO.Leave += new System.EventHandler(this.btnIO_Leave);
+            // 
+            // btnParameters
+            // 
+            this.btnParameters.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnParameters.FlatAppearance.BorderSize = 0;
+            this.btnParameters.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnParameters.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnParameters.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(240)))), ((int)(((byte)(255)))));
+            this.btnParameters.Location = new System.Drawing.Point(0, 244);
+            this.btnParameters.Name = "btnParameters";
+            this.btnParameters.Size = new System.Drawing.Size(186, 50);
+            this.btnParameters.TabIndex = 3;
+            this.btnParameters.Text = "Parameters";
+            this.btnParameters.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnParameters.UseVisualStyleBackColor = true;
+            this.btnParameters.Click += new System.EventHandler(this.btnParameters_Click);
+            this.btnParameters.Leave += new System.EventHandler(this.btnParameters_Leave);
+            // 
+            // btnLog
+            // 
+            this.btnLog.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLog.FlatAppearance.BorderSize = 0;
+            this.btnLog.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLog.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLog.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(240)))), ((int)(((byte)(255)))));
+            this.btnLog.Location = new System.Drawing.Point(0, 194);
+            this.btnLog.Name = "btnLog";
+            this.btnLog.Size = new System.Drawing.Size(186, 50);
+            this.btnLog.TabIndex = 2;
+            this.btnLog.Text = "Log";
+            this.btnLog.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnLog.UseVisualStyleBackColor = true;
+            this.btnLog.Click += new System.EventHandler(this.btnLog_Click);
+            this.btnLog.Leave += new System.EventHandler(this.btnLog_Leave);
+            // 
+            // btnHome
+            // 
+            this.btnHome.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnHome.FlatAppearance.BorderSize = 0;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHome.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(240)))), ((int)(((byte)(255)))));
+            this.btnHome.Location = new System.Drawing.Point(0, 144);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(186, 50);
+            this.btnHome.TabIndex = 1;
+            this.btnHome.Text = "Home";
+            this.btnHome.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            this.btnHome.Leave += new System.EventHandler(this.btnHome_Leave);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(186, 144);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(196)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(3, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Art of Automation";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AnıtArayüz.Properties.Resources.Untitled_1;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(137, 88);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // roundedButton1
+            // 
+            this.roundedButton1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.roundedButton1.BackgroundColor = System.Drawing.SystemColors.ActiveBorder;
+            this.roundedButton1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.roundedButton1.BorderRadius = 20;
+            this.roundedButton1.BorderSize = 0;
+            this.roundedButton1.FlatAppearance.BorderSize = 0;
+            this.roundedButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.roundedButton1.ForeColor = System.Drawing.Color.DarkRed;
+            this.roundedButton1.Location = new System.Drawing.Point(714, 0);
+            this.roundedButton1.Name = "roundedButton1";
+            this.roundedButton1.Size = new System.Drawing.Size(48, 36);
+            this.roundedButton1.TabIndex = 3;
+            this.roundedButton1.Text = "X";
+            this.roundedButton1.TextColor = System.Drawing.Color.DarkRed;
+            this.roundedButton1.UseVisualStyleBackColor = false;
+            this.roundedButton1.Click += new System.EventHandler(this.roundedButton1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(34)))), ((int)(((byte)(50)))));
+            this.panel3.Controls.Add(this.roundedButton1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(186, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(765, 40);
+            this.panel3.TabIndex = 4;
+            this.panel3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseDown);
+            this.panel3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseMove);
+            this.panel3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(951, 577);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnIO;
+        private System.Windows.Forms.Button btnParameters;
+        private System.Windows.Forms.Button btnLog;
+        private System.Windows.Forms.Panel pnlNav;
+        private RoundedButton roundedButton1;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace AnıtArayüz
+{
+    public partial class Form1 : Form
+    {
+        int mov;
+        int movX;
+        int movY;
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        
+        private static extern IntPtr CreateRoundRectRgn
+         (
+               int nLeftRect,
+               int nTopRect,
+               int nRightRect,
+               int nBottomRect,
+               int nWidthEllipse,
+               int nHeightEllipse
+         );
+
+        public Form1()
+        {
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnHome.Height;
+            pnlNav.Top = btnHome.Top;
+            pnlNav.Left = btnHome.Left;
+            btnHome.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void btnLog_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnLog.Height;
+            pnlNav.Top = btnLog.Top;
+            btnLog.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void btnParameters_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnParameters.Height;
+            pnlNav.Top = btnParameters.Top;
+            btnParameters.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void btnIO_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnIO.Height;
+            pnlNav.Top = btnIO.Top;
+            btnIO.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnSettings.Height;
+            pnlNav.Top = btnSettings.Top;
+            btnSettings.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void btnLog_Leave(object sender, EventArgs e)
+        {
+            btnLog.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnHome_Leave(object sender, EventArgs e)
+        {
+            btnHome.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnParameters_Leave(object sender, EventArgs e)
+        {
+            btnParameters.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnIO_Leave(object sender, EventArgs e)
+        {
+            btnIO.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnSettings_Leave(object sender, EventArgs e)
+        {
+            btnSettings.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+      
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnHome.PerformClick();
+        }
+
+        private void roundedButton1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void panel3_MouseDown(object sender, MouseEventArgs e)
+        {
+            mov = 1;
+            movX = e.X;
+            movY = e.Y;
+        }
+
+        private void panel3_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(mov == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - movX, MousePosition.Y - movY);
+            }
+        }
+
+        private void panel3_MouseUp(object sender, MouseEventArgs e)
+        {
+            mov = 0;
+        }
+    }
+}
